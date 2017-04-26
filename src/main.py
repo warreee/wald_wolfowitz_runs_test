6aa337b0cbe28038745c59c1e8664e65764b1f13
warreee <ward.schodts@student.kuleuven.be>
2017-04-26 22:28:11 +0200

    2 tests are failing probably due to an randomness error.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from scipy.special import ndtr
+from scipy.stats import norm
+import numpy as np
 
 
 def _calculate_p_value(z_value):
@@ -7,15 +8,92 @@ def _calculate_p_value(z_value):
     Here we assume using the normal distribution
     :return: 
     """
-    return ndtr(z_value)
+    return norm.sf(abs(z_value))
 
 
-def runs(list1, list2, confidence_interval=0.05):
+def runs(list1, list2):
     """
     This function executes the whole Wald-Wolfowitz runs tests.
     :param list1: 
     :param list2: 
-    :param confidence_interval: 
     :return: 
     """
-    return 0.1
+    labels = _get_runs_list(list1, list2)
+    nruns = _calculate_nruns(labels)
+    mean = _calculate_mean(len(list1), len(list2))
+    variance = _calculate_variance(mean, len(list1) + len(list2))
+    z_score = _calculate_z_score(nruns, mean, variance)
+    p_val = _calculate_p_value(z_score)
+    return p_val
+
+
+def _get_runs_list(list1, list2):
+    """
+    Given the 2 lists create the runs list.
+    i.e. label both lists and sort the concatenated lists and then return the list of labels
+    :param list1: 
+    :param list2: 
+    :return: 
+    """
+    l1 = _add_label_to_list(list1, "X")
+    l2 = _add_label_to_list(list2, "Y")
+    lst = l1 + l2
+    sorted_list = sorted(lst, key=lambda x: x[0])
+    return [l[1] for l in sorted_list]
+
+
+def _add_label_to_list(lst, label):
+    """
+    Given a list create a list of tuples of which the second element is the label.
+    :param lst: 
+    :param label: 
+    :return: 
+    """
+    return map(lambda x: (x, label), lst)
+
+
+def _calculate_z_score(nruns, mean, variance):
+    """
+    Calculates the z-score.
+    :return: 
+    """
+    return (nruns - mean) / np.sqrt(variance)
+
+
+def _calculate_mean(n_plus, n_min):
+    """
+    Calculates the mean, defined as:
+    (2 * n_plus * n_min)/N + 1
+    :param n_plus: 
+    :param n_min: 
+    :return: 
+    """
+    numerator = 2 * n_plus * n_min
+    denominator = n_plus + n_min
+    return numerator / float(denominator) + 1
+
+
+def _calculate_variance(mean, n):
+    """
+    Calculates the variance
+    :return: 
+    """
+    assert n > 1, "The number of elements should be higher than 1"
+    return ((mean - 1) * (mean - 2)) / float(n - 1)
+
+
+def _calculate_nruns(labels):
+    """
+    Calculate the number of runs.
+    A run is defined as the follow up of the same signs.
+    :return: int, the number of runs
+    """
+    nruns = 0
+    last_seen = None
+
+    for label in labels:
+        if label != last_seen:
+            nruns += 1
+        last_seen = label
+
+    return nruns
