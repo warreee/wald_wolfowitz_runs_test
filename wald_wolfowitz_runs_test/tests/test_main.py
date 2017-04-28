11e95009e9fd53af373e60b1cca7a2343972c826
warreee <ward.schodts@student.kuleuven.be>
2017-04-28 17:02:33 +0200

    fixed bug with p-values

--- wald_wolfowitz_runs_test/tests/test_main.py
+++ wald_wolfowitz_runs_test/tests/test_main.py
@@ -11,11 +11,11 @@ High level testing of the package.
 class TestMain(object):
     def test_calculate_p_value(self):
         p_val34 = _calculate_p_value(-3.4)
-        assert_almost_equal(.0003, p_val34, delta=0.0001)
+        assert_almost_equal(.9996, p_val34, delta=0.0001)
         p_val09 = _calculate_p_value(-0.9)
-        assert_almost_equal(.1841, p_val09, delta=0.0001)
+        assert_almost_equal((1-.1841), p_val09, delta=0.0001)
         p_val1 = _calculate_p_value(-1)
-        assert_almost_equal(.1587, p_val1, delta=0.0001)
+        assert_almost_equal((1-.1587), p_val1, delta=0.0001)
 
     def test_runs_chisquare(self):
         list_dist_A = np.random.chisquare(2, 100)
@@ -23,8 +23,8 @@ class TestMain(object):
         p_val = runs(list_dist_A, list_dist_B)
         assert_greater(p_val, 0.05)
 
-        list_dist_A = np.random.chisquare(1, 1000000)
-        list_dist_B = np.random.chisquare(1, 1000000)
+        list_dist_A = np.random.chisquare(1, 10000)
+        list_dist_B = np.random.chisquare(1, 10000)
         p_val = runs(list_dist_A, list_dist_B)
         p_val2 = runs(list_dist_B, list_dist_A)
         assert_equal(p_val, p_val2)
